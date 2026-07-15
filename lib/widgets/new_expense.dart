b00a8900c3011e9da09f06fd5501acbc89b5e832
IsaPortuguez <isabelportuguez.dev@gmail.com>
2026-07-15 17:50:37 -0600

    Implement expense submission callback

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -57,6 +59,16 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return; //We use it because we don't want to make an action after this dialog. We can't use the invalid information.
     }
+
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date:
+            _selectedDate!, //We add the ! to tell dart thet date is not null, because we already validate that
+        category: _selectedCategory,
+      ),
+    );
   }
 
   @override
