3cc6a68a4938b2a7555e5a30870124dc4e6c5367
IsaPortuguez <isabelportuguez.dev@gmail.com>
2026-07-16 14:38:44 -0600

    Add Dark Mode

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,27 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardThemeData(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -32,6 +50,7 @@ void main() {
           ),
         ),
       ), // It is used to centralize the app's global styles.
+      // themeMode: ThemeMode.system, //default
       home: Expenses(),
     ),
   );
